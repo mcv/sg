3256c88450368cb1872004b534b7dd6fa568ddc9
Martijn Vos <mcvmcv@xs4all.nl>
2016-02-23 21:02:22 +0100

    moved old components

--- app/app.js
+++ app/app.js
@@ -1,8 +1,6 @@
 require('angular');
 
-var app = angular.module('app', []);
-
-app.controller('MainController', function($scope) {
+angular.module('app', []).controller('MainController', function($scope) {
   $scope.message = 'Angular Works!!!!';
 
   console.log("it works!");
---
+++ app/map/map2.js
@@ -0,0 +1,53 @@
+
+angular.module('spacegame').directive("sgMap2", ['Planet', 'Fleet', function() {
+  return {
+    restrict: 'AE',
+    templateUrl: "js/directives/map2.html",
+//    template: '<div id="canvas" class="canvas" style="background-color: #000000; width: {{gameconfig.width}}px; height: {{gameconfig.height}}px"></div>',
+
+    controller: function($scope) {
+
+      $scope.mapconfig = {
+        fleetcolour: "#fff",
+        planetcolour: "#0ff",
+        planetradius: 10,
+        selectedfleetcolour: "#f0f",
+        selectedplanetcolour: "#090",
+        waypointcolour: "#c0c"
+      }
+
+      $scope.cfg = $scope.mapconfig;
+
+    },
+    link: function ($scope, iElement, iAttrs, controller, transcludeFn) {
+      //
+//      console.log("link 2");
+//
+//      console.log("setting style: "+$scope.gameconfig.height);
+//      iElement.style = {height: $scope.gameconfig.height, width: $scope.gameconfig.width};
+
+      $scope.waypointString = function(fleet) {
+        var wpstring = "M"+fleet.x+","+fleet.y;
+        angular.forEach(fleet.waypoints, function(wp) {
+          wpstring = wpstring + "L"+wp.x+","+wp.y;
+        });
+        return wpstring;
+      }
+
+      $scope.fleetRotate = function(fleet) {
+        if (fleet.waypoints != null && fleet.waypoints[0]!= null) {
+          return $scope.rotate(fleet, fleet.waypoints[0]);
+        }
+        else {
+          return 0;
+        }
+      }
+      $scope.rotate = function(origin, direction) {
+        var rad = Math.atan2(direction.y - origin.y, direction.x - origin.x);
+        var deg = rad*180/Math.PI - 90; // correction for our fleets pointing down rather than to the right
+        return deg;
+      }
+
+    }
+  }
+}]);
